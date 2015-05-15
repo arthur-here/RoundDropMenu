c63540030ba18a00370d4c0055d0b910195fae05
Arthur Myronenko <arthurmyronenko@MacBook-Air-Arthur-2014.local>
2015-05-15 15:13:30 +0300

    Added example

---
+++ Example/Round-Drop-Menu/Drop.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Round-Drop-Menu
+//
+//  Created by Arthur Myronenko on 15.05.15.
+//  Copyright (c) 2015 Arthur Myronenko. All rights reserved.
+//
+
+import UIKit
+
+class Drop: DropProtocol {
+    var title: String
+    var description: String
+    var image: UIImage?
+    
+    required init(title: String, description: String, image: UIImage? = nil) {
+        self.title = title
+        self.description = description
+        self.image = image
+    }
+}
---
+++ Example/Round-Drop-MenuTests/Round_Drop_MenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Round_Drop_MenuTests.swift
+//  Round-Drop-MenuTests
+//
+//  Created by Arthur Myronenko on 15.05.15.
+//  Copyright (c) 2015 Arthur Myronenko. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Round_Drop_MenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
